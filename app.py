55470c15d5054e36706935997c77b40918c12c7b
Matthew <mcookhome@gmail.com>
2014-10-08 12:34:28 -0400

    not working

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request
-
+from google import search
 
 # this makes app an instance of the Flask class
 # and it passed the special variable __name__ into
@@ -11,13 +11,21 @@ app = Flask(__name__)
 @app.route("/home", methods= ["GET", "POST"])
 @app.route("/",methods= ["GET", "POST"]) 
 def home():
-    if request.method== "GET":
-        pass
-        
-    else:
-        team=request.form["bar"]
-        print team
     return render_template("home.html")
+    
+    
+
+@app.route("/results",methods= ["GET", "POST"])
+def results():
+    L=[]
+    string = None
+    if request.method == 'POST':
+        if string == None:
+            string = request.form['bar']
+    for url in search (string, stop=10):
+        L.append(url)
+    return render_template("results.html", L =L)    
+    
 if __name__=="__main__":
     app.debug=True
     app.run() 
