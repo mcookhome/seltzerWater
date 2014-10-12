1eb9dc0e70c2c2563d407eec6291957077dac54d
Matthew Cook <mcookhome@gmail.com>
2014-10-11 23:44:09 -0400

    made minor changes, removed errors and doesnt return answers = to the query

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask,render_template,request
 from google import search
-import urllib,pickle,re
+import urllib2,pickle,re, time, threading,unicodedata
 from bs4 import BeautifulSoup
 from collections import Counter
-import operator,unicodedata
 # this makes app an instance of the Flask class
 # and it passed the special variable __name__ into
 # the constructor
@@ -13,8 +12,61 @@ app = Flask(__name__)
 h = open('diction.txt','rb')
 d= pickle.load(h)
 h.close()
+ 
+#used to work with timeout issues on beautifulsoup
 
+class Ticker(threading.Thread):
+  """A very simple thread that merely blocks for :attr:`interval` and sets a
+  :class:`threading.Event` when the :attr:`interval` has elapsed. It then waits
+  for the caller to unset this event before looping again.
 
+  Example use::
+
+    t = Ticker(1.0) # make a ticker
+    t.start() # start the ticker in a new thread
+    try:
+      while t.evt.wait(): # hang out til the time has elapsed
+        t.evt.clear() # tell the ticker to loop again
+        print time.time(), "FIRING!"
+    except:
+      t.stop() # tell the thread to stop
+      t.join() # wait til the thread actually dies
+
+  """
+  # SIGALRM based timing proved to be unreliable on various python installs,
+  # so we use a simple thread that blocks on sleep and sets a threading.Event
+  # when the timer expires, it does this forever.
+  def __init__(self, interval):
+    super(Ticker, self).__init__()
+    self.interval = interval
+    self.evt = threading.Event()
+    self.evt.clear()
+    self.should_run = threading.Event()
+    self.should_run.set()
+
+  def stop(self):
+    """Stop the this thread. You probably want to call :meth:`join` immediately
+    afterwards
+    """
+    self.should_run.clear()
+
+  def consume(self):
+    was_set = self.evt.is_set()
+    if was_set:
+      self.evt.clear()
+    return was_set
+
+  def run(self):
+    """The internal main method of this thread. Block for :attr:`interval`
+    seconds before setting :attr:`Ticker.evt`
+
+    .. warning::
+      Do not call this directly!  Instead call :meth:`start`.
+    """
+    while self.should_run.is_set():
+      time.sleep(self.interval)
+      self.evt.set()
+#end working with timeouts
 def notname(lis):
     x=0
     for sub in lis:
@@ -67,11 +119,24 @@ def who():
     i=0
     for url in search(query, stop=5):
         print "hello is this alive"
-        f=urllib.urlopen(url)
-        soup = BeautifulSoup(f.read())
-        f.close()
-        toParse += soup.getText()
-        i=i+1
+        f=urllib2.urlopen(url)
+
+        t = Ticker(2)
+        t.start()
+        try: 
+            while t.evt.wait():
+                lines = f.read()
+                print "f"
+                soup = BeautifulSoup(lines )
+                f.close()
+                toParse += soup.getText()
+                i=i+1
+                t.stop()
+                t.join()
+        except:
+            t.stop()
+            t.join()
+            print "too long :("
         if i>4:
             break
     print "im out"
@@ -93,6 +158,8 @@ def who():
         #print string
         common.append(string)
         #print common
+    while common[0] in query:
+      common.remove(common[0])
     #testing
     return render_template("who.html", L =L,common=common)  
 
@@ -105,8 +172,9 @@ def when():
         query=request.form['query']
     i=0
     for url in search(query, stop=5):
-        print "hello is this alive"
-        f=urllib.urlopen(url)
+        print url
+
+        f=urllib2.urlopen(url)
         soup = BeautifulSoup(f.read())
         f.close()
         toParse += soup.getText()
